f0ad295e3ca3415e7f26d3beaee657c6234a65a2
anshumanpanda55~ <pandaanshuman333@gmail.com>
2026-07-25 14:08:14 +0530

    printing stars

---
+++ printing_stars.java
@@ -0,0 +1,14 @@
+public class printing_stars{
+    public static void main (String[] args){
+        System.out.println("Printing Stars");
+        for(int i=5 ; i>0 ; i--){
+            for (int j=0 ; j<i ; j++)
+            System.out.print("*");
+            System.out.println();
+            
+            
+    
+        }
+        
+    }
+}
